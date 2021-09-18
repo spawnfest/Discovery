e7cf69c83da9695787708c579a135dc3d03bc7e7
ghostdsb <dibyanshu.bhoi@gmail.com>
2021-09-18 18:14:13 +0530

    feat: added create app modal

--- lib/discovery_web/live/page_live.ex
+++ lib/discovery_web/live/page_live.ex
@@ -4,22 +4,61 @@ defmodule DiscoveryWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{}, apps: [], selected_app: nil)}
+    {:ok,
+     assign(socket,
+       query: "",
+       results: %{},
+       apps: get_apps(),
+       selected_app: nil,
+       show_modal: "none",
+       can_create_app: true
+     )}
   end
 
   @impl true
-  def handle_event("create-app", %{"app-name" => app_name}, socket) do
-    app_name
-    |> create_app()
+  def handle_event("create-app", %{"app-name" => app_name} = _params, socket) do
+    if socket.assigns.can_create_app do
+      app_name
+      |> create_app()
+    end
+
+    socket =
+      socket
+      |> assign(can_create_app: false)
 
     {:noreply, socket}
   end
 
   @impl true
-  def handle_event("select-app", %{"app" => app_name} = params, socket) do
+  def handle_event("select-app", %{"app" => app_name} = _params, socket) do
     socket =
       socket
       |> assign(selected_app: app_name)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("show-modal", _params, socket) do
+    display =
+      case socket.assigns.show_modal do
+        "none" -> "block"
+        "block" -> "none"
+        _ -> "none"
+      end
+
+    socket =
+      socket
+      |> assign(show_modal: display, can_create_app: true)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("hide-modal", _params, socket) do
+    socket =
+      socket
+      |> assign(show_modal: "none", can_create_app: true)
 
     {:noreply, socket}
   end
@@ -30,9 +69,19 @@ defmodule DiscoveryWeb.PageLive do
 
     socket =
       socket
-      |> assign(apps: [app_name | socket.assigns.apps])
+      |> assign(apps: [app_name | socket.assigns.apps], show_modal: "none")
 
     {:noreply, socket}
+  end
+
+  defp get_apps do
+    # sample app names
+    [
+      "T3",
+      "Watchex",
+      "Nightwatch",
+      "WSGO"
+    ]
   end
 
   defp create_app(app_name) do
